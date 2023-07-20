2d564fab098e1819b0a98e180f8dd130bbb428ab
Alanek2012 <alan.przybylo@gmail.com>
2023-07-20 20:29:41 +0200

    CheckMonke 1.2.0 - my old source code sucks ass

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckMonke.Plugin
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.biotest05.gorillatag.checkmonke";
+		public const string Name = "CheckMonke";
+		public const string Version = "1.0.0";
+	}
+}
